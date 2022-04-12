041efbd33f8166292e0bcea12912ab8692f80ceb
TUF <TUF@LAPTOP-257ULQGP>
2022-04-13 01:19:57 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblBalls = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblBalls
+        '
+        Me.lblBalls.AutoSize = True
+        Me.lblBalls.Location = New System.Drawing.Point(171, 169)
+        Me.lblBalls.Name = "lblBalls"
+        Me.lblBalls.Size = New System.Drawing.Size(53, 20)
+        Me.lblBalls.TabIndex = 0
+        Me.lblBalls.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(379, 165)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Get balls"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblBalls)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblBalls As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intBall As Integer
+        Dim n As Integer
+        Dim m As Integer
+        Dim intBalls() As Integer = {0, 0, 0, 0, 0, 0}
+        Dim same As Boolean
+        Dim rand1 As New System.Random
+        lblBalls.Text = ""
+        'get numbers
+        For n = 0 To 5
+            Do
+                same = False
+                intBalls(n) = rand1.Next(49) + 1
+                For m = 0 To (n - 1)
+                    If (intBalls(n) = intBalls(m)) Then
+                        same = True
+                    End If
+                Next m
+            Loop Until same = False
+        Next n
+        'display results
+        For n = 0 To 5
+            intBall = intBalls(n)
+            lblBalls.Text = lblBalls.Text & "  " & intBall
+        Next n
+    End Sub
+End Class
